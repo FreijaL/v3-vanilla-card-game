346a9b7284db9f0a4ca006906deb32793a6063a0
Maja Gedda <maja.gedda@zocom.se>
2023-01-16 13:33:48 +0100

    color on suites

--- script.js
+++ script.js
@@ -9,28 +9,50 @@ function createDeck() {
     // 4 suits 13 valörer
     let deck = [];
     let suits = ['&spades;', '&hearts;', '&clubs;', '&diams;'];
+    // loopa över symbolerna
     for (let i = 0; i < suits.length; i++) {
+        // för varje symbol skall vi skapa 13 kort
+        // det gör vi via ännu en loop
         for (let j = 2; j < 15; j++) {
+            //skapar kort-objekt
+            // där vi lägger in symbol och värde
             let card = {
                 suite: suits[i],
-                value: j
+                value: j,
+                color: getColor(suits[i]),
+                displayValue: 'A'
             };
+            // för varje nytt kort bör vi pusha in det i vår lokala deck-array
             deck.push(card);
         }
     }
+    // logik för färg på suite
+    function getColor(suite) {
+        if (suite == '&hearts;' || suite == '&diams;') {
+            return 'red';
+        } else {
+            return 'black';
+        }
+    }
+
+    function getValue(value) {
+        // logik för valör på kort (A, J, D, M) 
+    }
+
+    // returnerar hela decket när funktionen är färdig
     return deck;
 };
 
 console.log(deck);
 
 function lower() {
-    // logik för när anv gissar på att nsäta kort är lägre
+    // logik för när anv gissar på att nästa kort är lägre
 }
 
 function equal() {
-    // logik för när anv gissar på att nsäta kort är lägre
+    // logik för när anv gissar på att nästa kort är densamma
 }
 
 function higher() {
-    // logik för när anv gissar på att nsäta kort är lägre
+    // logik för när anv gissar på att nsäta kort är högre
 }
